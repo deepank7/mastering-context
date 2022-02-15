82e72e7b12acb2119f64e9532a71cfc0e091d3a7
deepank7 <36792699+deepank7@users.noreply.github.com>
2022-02-15 19:09:33 +0530

    e-commerce example with with zustand, context and context-selector

---
+++ pages/ecommerce/ecommerce-zustand.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useAddToCart, useCartCount, useLogin, useLogout, useUser } from "./store-zustand";
+
+const LoginSection = () => {
+  const login = useLogin();
+  const logout = useLogout();
+  return (
+    <div>
+      <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
+};
+
+const UserSection = () => {
+  const user = useUser();
+  return <div>User: {user}</div>;
+};
+
+const AddToCartSection = () => {
+  const addToCart = useAddToCart();
+  return (
+    <div>
+      <button onClick={addToCart}>Add To Cart</button>
+    </div>
+  );
+};
+
+const CartCountSection = () => {
+  const cartCount = useCartCount();
+  return <div>Cart count: {cartCount}</div>;
+};
+
+const ZustandPage = () => {
+  return (
+    <div>
+      <LoginSection />
+      <UserSection />
+      <AddToCartSection />
+      <CartCountSection />
+    </div>
+  );
+};
+
+export default ZustandPage;

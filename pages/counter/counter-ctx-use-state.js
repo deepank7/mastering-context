f98a66cac35672da231c8a2c6e0413a023703330
deepank7 <36792699+deepank7@users.noreply.github.com>
2022-02-15 17:32:19 +0530

    state management using prop drilling, use context with state and with reducer

---
+++ pages/counter/counter-ctx-use-state.js
@@ -0,0 +1,41 @@
+import React, { useState, createContext, useContext } from "react";
+
+const CounterContext = createContext(null);
+
+const CounterContextProvider = ({ children }) => (
+  <CounterContext.Provider value={useState(0)}>
+    {children}
+  </CounterContext.Provider>
+);
+
+const Container = () => (
+  <div>
+    <AddOneButton />
+  </div>
+);
+
+const AddOneButton = () => {
+  const [, setCounter] = useContext(CounterContext);
+  return (
+    <div>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
+        Add One
+      </button>
+    </div>
+  );
+};
+
+const Counter = () => {
+  const [counter] = useContext(CounterContext);
+  return <div>Counter: {counter}</div>;
+};
+const CounterCtxUseState = () => {
+  return (
+    <CounterContextProvider>
+      <Container />
+      <Counter />
+    </CounterContextProvider>
+  );
+};
+
+export default CounterCtxUseState;
---
+++ pages/counter/counter-use-state.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const Container = ({ setCounter }) => (
+  <div>
+    <AddOneButton setCounter={setCounter} />
+  </div>
+);
+
+const AddOneButton = ({ setCounter }) => (
+  <div>
+    <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
+      Add One
+    </button>
+  </div>
+);
+
+const Counter = ({ counter }) => <div>Counter: {counter}</div>;
+
+const CounterUseState = () => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+      <Container setCounter={setCounter} />
+      <Counter counter={counter} />
+    </div>
+  );
+};
+
+export default CounterUseState;
